9562cfd5dc7e51c38a221c558cd60b2ecc4948c0
StphnLwnga <boywilder99@gmail.com>
2020-05-21 20:04:15 +0300

    LoopBack 4 REST API

---
+++ TODO.md
@@ -0,0 +1,12 @@
+- Run `lb4 datasource`
+
+  - `node_db`
+  - Select connector: MongoDB
+  - `host: localhost`
+  - `port: 27017`
+  - `database: node_db`
+
+- Run `lb4 controller` - `contact`
+
+- ID increment function may have to be added to post method in controller
+  `let contactId = 1; while(await this.contactRepository.exists(contactId)){ contactId ++; } contact.id = contactId;`
---
+++ src/models/contact.model.ts
@@ -0,0 +1,56 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model({settings: {strict: false}})
+export class Contact extends Entity {
+  @property({
+    type: 'string',
+    required: true,
+  })
+  firstName: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  lastName: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  email: string;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  phoneNumber: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  company: string;
+
+  @property({
+    type: 'date',
+    required: true,
+  })
+  createdAt: string;
+
+  // Define well-known properties here
+
+  // Indexer property to allow additional data
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [prop: string]: any;
+
+  constructor(data?: Partial<Contact>) {
+    super(data);
+  }
+}
+
+export interface ContactRelations {
+  // describe navigational properties here
+}
+
+export type ContactWithRelations = Contact & ContactRelations;
